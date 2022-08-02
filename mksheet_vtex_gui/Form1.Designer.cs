5f6b678b7a1f7d1f84181fb86570d1214924c209
ron85 <ron85@DESKTOP-KQPPADT>
2022-08-01 18:58:56 -0700

    Add project files.

---
+++ mksheet_vtex_gui/Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace mksheet_vtex_gui
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tf2FolderTextBox = new System.Windows.Forms.TextBox();
+            this.folderBrowserDialog2 = new System.Windows.Forms.FolderBrowserDialog();
+            this.label2 = new System.Windows.Forms.Label();
+            this.frameTextBox = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.prefixTextBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.makeMKS = new System.Windows.Forms.Button();
+            this.loopBox = new System.Windows.Forms.CheckBox();
+            this.makeVTF = new System.Windows.Forms.Button();
+            this.statusLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.HelpRequest += new System.EventHandler(this.folderBrowserDialog1_HelpRequest);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(574, 6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(31, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(125, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Team Fortress 2 folder:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // tf2FolderTextBox
+            // 
+            this.tf2FolderTextBox.Location = new System.Drawing.Point(184, 6);
+            this.tf2FolderTextBox.Name = "tf2FolderTextBox";
+            this.tf2FolderTextBox.Size = new System.Drawing.Size(384, 23);
+            this.tf2FolderTextBox.TabIndex = 2;
+            this.tf2FolderTextBox.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(166, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Folder containing TGA frames:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // frameTextBox
+            // 
+            this.frameTextBox.Location = new System.Drawing.Point(184, 35);
+            this.frameTextBox.Name = "frameTextBox";
+            this.frameTextBox.Size = new System.Drawing.Size(384, 23);
+            this.frameTextBox.TabIndex = 4;
+            this.frameTextBox.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(574, 35);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(31, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "...";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 67);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(125, 15);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Frame filename prefix:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // prefixTextBox
+            // 
+            this.prefixTextBox.Location = new System.Drawing.Point(184, 64);
+            this.prefixTextBox.Name = "prefixTextBox";
+            this.prefixTextBox.Size = new System.Drawing.Size(421, 23);
+            this.prefixTextBox.TabIndex = 7;
+            this.prefixTextBox.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 96);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(114, 15);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Desired sprite name:";
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(184, 93);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(298, 23);
+            this.nameTextBox.TabIndex = 9;
+            this.nameTextBox.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // makeMKS
+            // 
+            this.makeMKS.Location = new System.Drawing.Point(12, 122);
+            this.makeMKS.Name = "makeMKS";
+            this.makeMKS.Size = new System.Drawing.Size(593, 23);
+            this.makeMKS.TabIndex = 10;
+            this.makeMKS.Text = "Create MKS file";
+            this.makeMKS.UseVisualStyleBackColor = true;
+            this.makeMKS.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // loopBox
+            // 
+            this.loopBox.AutoSize = true;
+            this.loopBox.Location = new System.Drawing.Point(488, 97);
+            this.loopBox.Name = "loopBox";
+            this.loopBox.Size = new System.Drawing.Size(117, 19);
+            this.loopBox.TabIndex = 11;
+            this.loopBox.Text = "Loop Animation?";
+            this.loopBox.UseVisualStyleBackColor = true;
+            this.loopBox.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // makeVTF
+            // 
+            this.makeVTF.Enabled = false;
+            this.makeVTF.Location = new System.Drawing.Point(12, 151);
+            this.makeVTF.Name = "makeVTF";
+            this.makeVTF.Size = new System.Drawing.Size(593, 23);
+            this.makeVTF.TabIndex = 12;
+            this.makeVTF.Text = "Create VTF file";
+            this.makeVTF.UseVisualStyleBackColor = true;
+            this.makeVTF.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.AutoSize = true;
+            this.statusLabel.Location = new System.Drawing.Point(12, 190);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(0, 15);
+            this.statusLabel.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(617, 214);
+            this.Controls.Add(this.statusLabel);
+            this.Controls.Add(this.makeVTF);
+            this.Controls.Add(this.loopBox);
+            this.Controls.Add(this.makeMKS);
+            this.Controls.Add(this.nameTextBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.prefixTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.frameTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tf2FolderTextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Animated Sprite Utility 1.0";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private FolderBrowserDialog folderBrowserDialog1;
+        private Button button1;
+        private Label label1;
+        private TextBox tf2FolderTextBox;
+        private FolderBrowserDialog folderBrowserDialog2;
+        private Label label2;
+        private TextBox frameTextBox;
+        private Button button2;
+        private Label label3;
+        private TextBox prefixTextBox;
+        private Label label4;
+        private TextBox nameTextBox;
+        private Button makeMKS;
+        private CheckBox loopBox;
+        private Button makeVTF;
+        private Label statusLabel;
+    }
+}
---
+++ mksheet_vtex_gui/Form1.cs
@@ -0,0 +1,240 @@
+using System.IO;
+
+namespace mksheet_vtex_gui
+{
+    public partial class Form1 : Form
+    {
+        private string tf2Folder;
+        private string frameFolder;
+        private string fileName;
+        private string mksFile;
+        private List<string> frames = new List<string>();
+
+        public Form1()
+        {
+            tf2Folder = Properties.Settings.Default.TeamFortressFolder;
+            frameFolder = Properties.Settings.Default.FrameFolder;
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            tf2FolderTextBox.Text = tf2Folder;
+            frameTextBox.Text = frameFolder;
+        }
+
+        private void folderBrowserDialog1_HelpRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            DialogResult dialogResult = folderBrowserDialog1.ShowDialog();
+            if (dialogResult == DialogResult.OK)
+            {
+                tf2Folder = folderBrowserDialog1.SelectedPath;
+
+                //Save to settings
+                Properties.Settings.Default.TeamFortressFolder = tf2Folder;
+                Properties.Settings.Default.Save();
+
+                tf2FolderTextBox.Text = tf2Folder;
+            }
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            tf2Folder = tf2FolderTextBox.Text;
+
+            //Save to settings
+            Properties.Settings.Default.TeamFortressFolder = tf2Folder;
+            Properties.Settings.Default.Save();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DialogResult dialogResult = folderBrowserDialog2.ShowDialog();
+            if (dialogResult == DialogResult.OK)
+            {
+                frameFolder = folderBrowserDialog2.SelectedPath;
+
+                //Save to settings
+                Properties.Settings.Default.FrameFolder = frameFolder;
+                Properties.Settings.Default.Save();
+
+                frameTextBox.Text = frameFolder;
+            }
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            //Filter out illegal characters
+            prefixTextBox.Text = CleanFileName(prefixTextBox.Text);
+            prefixTextBox.SelectionStart = prefixTextBox.Text.Length;
+        }
+
+        public string CleanFileName(string name)
+        {
+            //Removes invalid file name characters from given string
+            return string.Concat(name.Split(Path.GetInvalidFileNameChars()));
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+            //Filter out illegal characters
+            nameTextBox.Text = CleanFileName(nameTextBox.Text);
+            nameTextBox.SelectionStart = nameTextBox.Text.Length;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            fileName = nameTextBox.Text;
+
+            //Validate file names
+            if (fileName == "")
+            {
+                MessageBox.Show("Please enter a desired name for your VTF.");
+                return;
+            }
+            if (!Directory.Exists(tf2Folder))
+            {
+                MessageBox.Show(tf2Folder+" is not a valid directory.");
+                return;
+            }
+            if (!Directory.Exists(frameFolder))
+            {
+                MessageBox.Show(frameFolder + " is not a valid directory.");
+                return;
+            }
+
+            statusLabel.Text = "Creating MKS file...";
+
+            //Find all TGA files with the appropriate prefix
+            string[] files = Directory.GetFiles(frameFolder);
+            frames.Clear();
+            foreach (string file in files) //Find all tga files beginning with provided prefix
+            {
+                if (Path.GetFileName(file).StartsWith(prefixTextBox.Text) && Path.GetExtension(file).ToLower().Equals(".tga"))
+                {
+                    frames.Add(Path.GetFileName(file));
+                }
+            }
+            
+            if(frames.Count == 0)
+            {
+                MessageBox.Show("No frames with prefix " + prefixTextBox.Text + " found.");
+                return;
+            }
+
+            frames.Sort(); //Sort frames. Assumption is that frames are provided with ascending numerical suffixes.
+
+            //Create mks file
+            mksFile = frameFolder + "\\" + fileName + ".mks";
+            using FileStream fs = File.Create(mksFile);
+            fs.Write(System.Text.Encoding.UTF8.GetBytes("sequence 0"));
+            if (loopBox.Checked)
+            {
+                fs.Write(System.Text.Encoding.UTF8.GetBytes("\r\nLOOP"));
+            }
+            foreach (string frame in frames)
+            {
+                fs.Write(System.Text.Encoding.UTF8.GetBytes("\r\nframe "+frame+" 1"));
+            }
+
+            //Enable "Create VTF File" button
+            makeVTF.Enabled = true;
+            statusLabel.Text = "Done! " + fileName + ".mks created. Make any changes now.";
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            frameFolder = frameTextBox.Text;
+            //Save to settings
+            Properties.Settings.Default.FrameFolder = frameFolder;
+            Properties.Settings.Default.Save();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            statusLabel.Text = "Creating SHT and TGA files...";
+
+            //Copy/move all relevant files to /bin
+            string tf2Bin = tf2Folder + "\\bin\\";
+            foreach(string frame in frames)
+            {
+                File.Copy(frameFolder + "\\" + frame, tf2Bin + frame);
+            }
+            File.Move(mksFile, tf2Bin + fileName+".mks");
+
+            //Create sht and tga
+            var processStartInfo = new System.Diagnostics.ProcessStartInfo();
+            processStartInfo.WorkingDirectory = tf2Folder+"\\bin";
+            processStartInfo.FileName = "cmd.exe";
+            processStartInfo.Arguments = "/C mksheet.exe "+fileName+".mks";
+            System.Diagnostics.Process mksheet = System.Diagnostics.Process.Start(processStartInfo);
+            mksheet.WaitForExit();
+
+            //Delete files that are no longer needed
+            foreach (string frame in frames) 
+            {
+                File.Delete(tf2Bin + frame);
+            }
+            File.Delete(tf2Bin + fileName + ".mks");
+
+            //If materialsrc does not yet exist, create it
+            if (!File.Exists(tf2Folder + "\\tf\\materialsrc"))
+            {
+                _ = Directory.CreateDirectory(tf2Folder + "\\tf\\materialsrc");
+            }
+
+            //Move tga and sht files to materialsrc
+            File.Move(tf2Bin + fileName + ".sht", tf2Folder + "\\tf\\materialsrc\\" + fileName + ".sht");
+            File.Move(tf2Bin + fileName + ".tga", tf2Folder + "\\tf\\materialsrc\\" + fileName + ".tga");
+
+            processStartInfo.WorkingDirectory = Environment.GetEnvironmentVariable("SYSTEMROOT");
+
+            statusLabel.Text = "Creating VTF file...";
+
+            //Set envars and run vtex
+            processStartInfo.Arguments = "/C set VGAME="+tf2Folder + "&set VPROJECT=" + tf2Folder + "\\tf" + "&\""+ tf2Bin + "vtex.exe\" \"" + tf2Folder + "\\tf\\materialsrc\\" + fileName + ".sht\"";
+            System.Diagnostics.Process vtex = System.Diagnostics.Process.Start(processStartInfo);
+            vtex.WaitForExit();
+
+            //Delete files that are no longer needed
+            File.Delete(tf2Folder + "\\tf\\materialsrc\\" + fileName + ".sht");
+            File.Delete(tf2Folder + "\\tf\\materialsrc\\" + fileName + ".tga");
+
+            //Move completed vtf to the original directory
+            File.Move(tf2Folder + "\\tf\\materials\\" + fileName + ".vtf", frameFolder + "\\" + fileName + ".vtf");
+
+            statusLabel.Text = "Done! " + fileName + ".vtf created.";
+        }
+    }
+}
